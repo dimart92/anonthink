cfa9974a6656636af40a90623e53441af1b9d03c
dimart92 <dimart92sw@gmail.com>
2025-09-15 22:56:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
-const channelId = process.env.CHANNEL_ID;   // строка "-100..."
+const channelId = process.env.CHANNEL_ID;          // строка "-1002909199388"
 
-bot.start(ctx => ctx.reply('Отправь мне anything → опубликую анонимно.'));
+bot.start(ctx => ctx.reply('Отправь anything → опубликую анонимно.'));
 bot.on('text', ctx => ctx.telegram.sendMessage(channelId, ctx.message.text));
 bot.on('photo', ctx => ctx.telegram.sendPhoto(channelId, ctx.message.photo.at(-1).file_id,
                                             { caption: ctx.message.caption || '' }));
@@ -12,5 +12,5 @@ bot.on('video', ctx => ctx.telegram.sendVideo(channelId, ctx.message.video.file_
 bot.on('document', ctx => ctx.telegram.sendDocument(channelId, ctx.message.document.file_id,
                                                    { caption: ctx.message.caption || '' }));
 
-// Vercel serverless-entrypoint
+// экспортируем обработчик, который Vercel вызовет по пути /webhook/...
 module.exports = bot.webhookCallback(`/webhook/${process.env.TELEGRAM_TOKEN}`);
